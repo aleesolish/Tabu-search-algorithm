ff67d3c74ff9f374e31cf0c870d5d17ce91b55f4
Diego Plata <diegogplata@gmail.com>
2020-03-16 22:55:04 -0600

    map configuration ready

--- tabusearch.py
+++ tabusearch.py
@@ -8,6 +8,7 @@ mex = Node('mex')
 cva = Node('cva')
 qro = Node('qro')
 slp = Node('slp')
+gto = Node('gto')
 
 mex_cva = Edge(mex, cva, 100)
 mex_qro = Edge(mex, qro, 200)
@@ -15,16 +16,23 @@ mex_qro = Edge(mex, qro, 200)
 cva_mex = Edge(cva, mex, 100)
 
 qro_mex = Edge(qro, mex, 200)
+qro_gto = Edge(qro, gto, 100)
 qro_slp = Edge(qro, slp, 250)
 
 slp_qro = Edge(slp, qro, 250)
+slp_gto = Edge(slp, gto, 150)
+
+gto_qro = Edge(gto, qro, 100)
+gto_slp = Edge(gto, slp, 150)
 
 mex_list = [mex_cva, mex_qro]
 cva_list = [cva_mex]
-qro_list = [qro_mex, qro_slp]
-slp_list = [slp_qro]
+qro_list = [qro_mex, qro_slp, qro_gto]
+slp_list = [slp_qro, slp_gto]
+gto_list = [gto_qro, gto_slp]
 
-cities = [mex_list, cva_list, qro_list, slp_list]
+cities = [mex, cva, qro, slp, gto]
+cities_adjacencies = [mex_list, cva_list, qro_list, slp_list, gto_list]
 
 def get_adjacency(edge):
     return (edge.get_node_two().get_name(), edge.get_weight())
@@ -35,5 +43,15 @@ def state_neighborhood(adjacency_list):
         return_list.append(get_adjacency(adjacency))
     return return_list
 
-for city in cities:
-    print(state_neighborhood(city))
+def get_map(cities, cities_adjacencies):
+    map = []
+    for city, adjacency in zip(cities, cities_adjacencies):
+        dictionary = {
+        'name': city.get_name(),
+        'adjacencies': state_neighborhood(adjacency)
+        }
+        map.append(dictionary)
+    return map
+
+map = get_map(cities, cities_adjacencies)
+print(map)
