9c6d0f358402e00858c2d1efe216381081ad0ff8
Diego Plata <diegogplata@gmail.com>
2020-03-18 10:42:52 -0600

    commentaries added

--- tabusearch.py
+++ tabusearch.py
@@ -35,15 +35,21 @@ gto_list = [gto_qro, gto_slp]
 cities = [cva, mex, qro, slp, gto]
 cities_adjacencies = [cva_list, mex_list, qro_list, slp_list, gto_list]
 
+
+#FUNCTION THAT RETURNS ADJACENCIES VALUES NAME, WEIGHT
 def get_adjacency(edge):
     return (edge.get_node_two().get_name(), edge.get_weight())
 
+
+#FUNCTION TO APPEND ADJACENCIES WITH NAME AND WEIGHT TO EVERY CITY ('',1)
 def state_neighborhood(adjacency_list):
     return_list = []
     for adjacency in adjacency_list:
         return_list.append(get_adjacency(adjacency))
     return return_list
 
+
+#FUNCTION TO CREATE A LIST OF DICTIONARIES RELATIVE TO EVERY CITY {NAME: , ADJACENCIES:}
 def set_map(cities, cities_adjacencies):
     map = []
     for city, adjacency in zip(cities, cities_adjacencies):
@@ -54,6 +60,8 @@ def set_map(cities, cities_adjacencies):
         map.append(dictionary)
     return map
 
+
+#FUNCTION FOR FIRST SOLUTION
 def first_run(map):
     start_node = map[0]
     end_node = map[-1]
@@ -73,7 +81,6 @@ def first_run(map):
                     if city['name'] == adjacency[0]:
                         next_best = city
                         distance_sum = adjacency[1]
-                #print(min_distance, next_best)
 
 
         total_first_distance = total_first_distance + distance_sum
@@ -86,6 +93,5 @@ def first_run(map):
 
 
 map = set_map(cities, cities_adjacencies)
-#print(map)
 first_solution, first_total_distance = first_run(map)
 print(first_solution, first_total_distance)
