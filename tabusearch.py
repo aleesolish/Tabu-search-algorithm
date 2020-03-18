0a92efcb57d84c0fe080c1c6255db2db7ea66a8c
Diego Plata <diegogplata@gmail.com>
2020-03-18 13:34:38 -0600

    first solution now working fine, changed node and edges names

--- tabusearch.py
+++ tabusearch.py
@@ -1,39 +1,49 @@
 from classes import *
-import math
 
 """
-mx : [("cva", 100), ("qro", 150), ("slp", 500), ("acpco", 500)]
+ie.
+a : [('b', 100), ('c', 150), ('d', 500), ('e', 500)]
 """
 
-mex = Node('mex')
-cva = Node('cva')
-qro = Node('qro')
-slp = Node('slp')
-gto = Node('gto')
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
 
-mex_cva = Edge(mex, cva, 100)
-mex_qro = Edge(mex, qro, 200)
+a_b = Edge(a, b, 20)
+a_c = Edge(a, c, 18)
+a_d = Edge(a, d, 22)
+a_e = Edge(a, e, 26)
 
-cva_mex = Edge(cva, mex, 100)
+b_a = Edge(b, a, 20)
+b_c = Edge(b, c, 10)
+b_d = Edge(b, d, 11)
+b_e = Edge(b, e, 12)
 
-qro_mex = Edge(qro, mex, 200)
-qro_gto = Edge(qro, gto, 100)
-qro_slp = Edge(qro, slp, 250)
+c_a = Edge(c, a, 18)
+c_b = Edge(c, b, 10)
+c_d = Edge(c, d, 23)
+c_e = Edge(c, e, 24)
 
-slp_qro = Edge(slp, qro, 250)
-slp_gto = Edge(slp, gto, 150)
+d_a = Edge(d, a, 22)
+d_b = Edge(d, b, 11)
+d_c = Edge(d, c, 23)
+d_e = Edge(d, e, 40)
 
-gto_qro = Edge(gto, qro, 100)
-gto_slp = Edge(gto, slp, 150)
+e_a = Edge(e, a, 26)
+e_b = Edge(e, b, 12)
+e_c = Edge(e, c, 24)
+e_d = Edge(e, d, 40)
 
-mex_list = [mex_cva, mex_qro]
-cva_list = [cva_mex]
-qro_list = [qro_mex, qro_slp, qro_gto]
-slp_list = [slp_qro, slp_gto]
-gto_list = [gto_qro, gto_slp]
+a_list = [a_b, a_c, a_d, a_e]
+b_list = [b_a, b_c, b_d, b_e]
+c_list = [c_a, c_b, c_d, c_e]
+d_list = [d_a, d_b, d_c, d_e]
+e_list = [e_a, e_b, e_c, e_d]
 
-cities = [cva, mex, qro, slp, gto]
-cities_adjacencies = [cva_list, mex_list, qro_list, slp_list, gto_list]
+nodes = [a, b, c, d, e]
+edges = [a_list, b_list, c_list, d_list, e_list]
 
 
 #FUNCTION THAT RETURNS ADJACENCIES VALUES NAME, WEIGHT
@@ -41,21 +51,21 @@ def get_adjacency(edge):
     return (edge.get_node_two().get_name(), edge.get_weight())
 
 
-#FUNCTION TO APPEND ADJACENCIES WITH NAME AND WEIGHT TO EVERY CITY ('',1)
-def state_neighborhood(adjacency_list):
+#FUNCTION TO APPEND ADJACENCIES WITH NAME AND WEIGHT TO EVERY NODE ('',1)
+def state_neighborhood(edges):
     return_list = []
-    for adjacency in adjacency_list:
-        return_list.append(get_adjacency(adjacency))
+    for edge in edges:
+        return_list.append(get_adjacency(edge))
     return return_list
 
 
-#FUNCTION TO CREATE A LIST OF DICTIONARIES RELATIVE TO EVERY CITY {NAME: , ADJACENCIES:}
-def set_map(cities, cities_adjacencies):
+#FUNCTION TO CREATE A LIST OF DICTIONARIES RELATIVE TO EVERY NODE {NAME: , EDGES:}
+def set_map(nodes, edges):
     map = []
-    for city, adjacency in zip(cities, cities_adjacencies):
+    for node, edge in zip(nodes, edges):
         dictionary = {
-        'name': city.get_name(),
-        'adjacencies': state_neighborhood(adjacency)
+        'node': node.get_name(),
+        'edges': state_neighborhood(edge)
         }
         map.append(dictionary)
     return map
@@ -64,34 +74,39 @@ def set_map(cities, cities_adjacencies):
 #FUNCTION FOR FIRST SOLUTION
 def first_run(map):
     start_node = map[0]
-    end_node = map[-1]
+    end_node = start_node
     visiting_node = start_node
     previous_node = start_node
     first_solution = []
     total_first_distance = 0
 
+    #while end_node not in first_solution:
     while visiting_node not in first_solution:
-        min_distance = math.inf
+        min_distance = 1000000
         #for loop to determine what adjacency has the lower distance
-        for adjacency in visiting_node['adjacencies']:
-            #conditional to assign new minimal distance
-            if adjacency[1] < min_distance and adjacency is not previous_node: #not in first_solution:
-                #for loop to find the next best node
-                for city in map:
-                    if city['name'] == adjacency[0]:
-                        next_best = city
-                        distance_sum = adjacency[1]
+        for adjacency in visiting_node['edges']:
+            #for loop to find the next best node
+            for node in map:
+                if node['node'] == adjacency[0]:
+                    #conditional to assign new minimal distance
+                    if adjacency[1] < min_distance and node not in first_solution:
+                        min_distance = adjacency[1]
+                        next_best = node
 
-
-        total_first_distance = total_first_distance + distance_sum
         first_solution.append(visiting_node)
-        previous_node = visiting_node
         visiting_node = next_best
+        total_first_distance = total_first_distance + min_distance
+
+    first_solution.append(end_node)
+    total_first_distance = total_first_distance - min_distance
 
     return first_solution, total_first_distance
 
 
 
-map = set_map(cities, cities_adjacencies)
+
+
+map = set_map(nodes, edges)
+print(map)
 first_solution, first_total_distance = first_run(map)
 print(first_solution, first_total_distance)
